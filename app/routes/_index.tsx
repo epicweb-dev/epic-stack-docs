6d5f76441ee30ecc3df3f304a703d63dfefd4925
Shane <shane@swalker.dev>
2023-11-03 08:36:37 -0500

    added title to landing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,10 +16,13 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Index() {
 	const data = useLoaderData<typeof loader>()
-	const { code } = data
+	const { code, frontmatter } = data
 	const Component = useMdxComponent(code)
 	return (
 		<div className="container pt-3">
+			<h2 className="pb-2 text-xl font-bold md:text-3xl xl:text-5xl">
+				{frontmatter.title}
+			</h2>
 			<div className="max-w-4xl">
 				<div className="prose sm:prose-lg xl:prose-2xl prose-light dark:prose-dark">
 					<Component />
