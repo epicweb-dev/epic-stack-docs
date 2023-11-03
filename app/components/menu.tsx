f5c40cd1a19821bcd33a1624f0dd99c2371c40a0
Shane <shane@swalker.dev>
2023-11-02 22:26:53 -0500

    added close logic to mobile menu

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,14 +1,22 @@
 import { Link } from '@remix-run/react'
+import { useRef } from 'react'
 import { useTheme } from '#app/root.tsx'
 import staticCategories from '#content/docs/categories.ts'
 import { SearchBar } from './search-bar.tsx'
-import { Dialog, DialogTrigger, DialogContent } from './ui/dialog.tsx'
+import {
+	Dialog,
+	DialogTrigger,
+	DialogContent,
+	DialogClose,
+} from './ui/dialog.tsx'
 import { Icon } from './ui/icon.tsx'
 
 const CategoryLinks = ({
 	categories,
+	onClick,
 }: {
 	categories: typeof staticCategories
+	onClick?: () => void
 }) => {
 	return (
 		<>
@@ -20,7 +28,10 @@ const CategoryLinks = ({
 							{subcategories.map(subcategory => {
 								return (
 									<div className="ml-2" key={subcategory}>
-										<Link to={`/topic/${subcategory.split('.')[0]}`}>
+										<Link
+											to={`/topic/${subcategory.split('.')[0]}`}
+											onClick={onClick}
+										>
 											<h5 className="text-lg font-thin">
 												{subcategory.split('.')[0]}
 											</h5>
@@ -38,6 +49,10 @@ const CategoryLinks = ({
 
 export default function Menu() {
 	const theme = useTheme()
+	const closeButtonRef = useRef<HTMLButtonElement>(null)
+	const closeDialog = () => {
+		closeButtonRef.current?.click()
+	}
 	return (
 		<nav className="relative">
 			<div className="sticky top-0 hidden h-[100vh] w-48 md:flex lg:w-64">
@@ -48,10 +63,10 @@ export default function Menu() {
 							className="h-32 w-32"
 						/>
 					</Link>
-					<div className="flex flex-col gap-2 overflow-scroll py-2">
+					<div className="flex flex-col gap-2 overflow-scroll p-2">
 						<Link
 							to="/topic/getting-started"
-							className={` my-2 rounded-sm ${
+							className={`my-2 rounded-sm md:text-base ${
 								theme === 'light' ? 'bg-[#6A23FF]' : 'bg-[#D6C5FF]'
 							} py-2 text-center text-2xl font-semibold text-primary-foreground ${
 								theme === 'light'
@@ -99,11 +114,19 @@ export default function Menu() {
 								</Link>
 								<h3 className="py-2 text-2xl font-semibold">Topics</h3>
 								<div className="flex flex-col gap-2">
-									<CategoryLinks categories={staticCategories} />
+									<CategoryLinks
+										categories={staticCategories}
+										onClick={closeDialog}
+									/>
 								</div>
 							</div>
 						</div>
 					</DialogContent>
+					<DialogClose id="dialog-close" asChild>
+						<button ref={closeButtonRef} className="hidden">
+							Close
+						</button>
+					</DialogClose>
 				</Dialog>
 				<SearchBar autoSubmit status="idle" />
 			</div>
